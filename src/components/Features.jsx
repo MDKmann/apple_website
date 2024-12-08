bd2d6eaf65ad3d4dfd3d983c6739c9e66412768f
Michael <MDKuhlmann@zoho.com>
2024-12-07 21:29:17 -0600

    added features section with videos, paragraphs, and animations

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,9 +1,122 @@
-import React from 'react'
+import { useGSAP } from "@gsap/react";
+import gsap from 'gsap';
+import React, { useRef } from "react";
+import { animateWithGsap } from "../utils/animations";
+import { exploreVideo, explore1Img, explore2Img } from "../utils";
 
 const Features = () => {
-  return (
-    <div>Features</div>
-  )
-}
+  const videoRef = useRef();
 
-export default Features
+  useGSAP(() => {
+    gsap.to("#exploreVideo", {
+      scrollTrigger: {
+        trigger: "#exploreVideo",
+        toggleActions: "play pause reverse restart",
+        start: " -10% bottom",
+      },
+      onComplete: () => {
+        videoRef.current.play();
+      },
+    });
+
+    animateWithGsap("#features_title", {
+      y: 0,
+      opacity: 1,
+    });
+    animateWithGsap(
+      ".g_grow",
+      {
+        scale: 1,
+        opacity: 1,
+        ease: "power1",
+      },
+      { scrub: 5.5 },
+    );
+    animateWithGsap(".g_text", {
+      y: 0,
+      opacity: 1,
+      ease: "power2.inOut",
+      duration: 1,
+    });
+  }, []);
+
+  return (
+    <section className="relative h-full overflow-hidden common-padding bg-zinc">
+      <div className="screen-max-width">
+        <div className="w-full mb-12">
+          <h1 id="features_title" className="section-heading">
+            Explore the full story.
+          </h1>
+        </div>
+
+        <div className="flex flex-col items-center justify-center overflow-hidden">
+          <div className="pl-24 mt-32 mb-32">
+            <h2 className="text-5xl font-semibold lg:text-7xl">iPhone.</h2>
+            <h2 className="text-5xl font-semibold lg:text-7xl">
+              Forged in titanium.
+            </h2>
+          </div>
+
+          <div className="flex-col flex-center sm:px-10">
+            <div className="relative flex items-center w-full h-[50vh]">
+              <video
+                playsInline
+                id="exploreVideo"
+                className="object-cover object-center w-full h-full"
+                preload="none"
+                muted
+                autoPlay
+                ref={videoRef}
+              >
+                <source src={exploreVideo} type="video/mp4" />
+              </video>
+            </div>
+
+            <div className="relative flex flex-col w-full">
+              <div className="feature-video-container">
+                <div className="flex-1 overflow-hidden h-[50vh]">
+                  <img
+                    src={explore1Img}
+                    alt="titanium"
+                    className="feature-video g_grow"
+                  />
+                </div>
+                <div className="flex-1 overflow-hidden h-[50vh]">
+                  <img
+                    src={explore2Img}
+                    alt="titanium 2"
+                    className="feature-video g_grow"
+                  />
+                </div>
+              </div>
+
+              <div className="feature-text-container">
+                <div className="flex-1 flex-center">
+                  <p className="feature-text g_text">
+                    iPhone 15 Pro is {""}
+                    <span className="text-white">
+                      the first iPhone to feature an aerospace-grade titanium
+                      design
+                    </span>
+                    , using the same allow that spacecrafts use for missions to
+                    Mars.
+                  </p>
+                </div>
+                <div className="flex-1 flex-center">
+                  <p className="feature-text g_text">
+                    Titanium has one of the best strength-to-weight ratios of
+                    any metal, make these our {""}
+                    <span className="text-white">lightest Pro models ever</span>
+                    . You'll notice the difference the moment you pick one up.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Features;
